c00e992779254c0c0a0c073ef527e753c81446b3
dmitriydmx <dmitriydmx@mail.ru>
2024-02-08 20:47:47 +0900

    second commit classes

---
+++ src/js/class/Character.js
@@ -0,0 +1,20 @@
+class Character {
+  constructor(name, type) {
+    const types = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
+    if (name.length < 2 || name.length > 10) {
+      throw new Error('Имя должно содержать от 2 до 10 символов');
+    }
+    if (!types.includes(type)) {
+      throw new Error('Выбран некорректный класс персонажа');
+    }
+    
+    this.name = name;
+    this.type = type;
+    this.health = 100;
+    this.level = 1;
+    this.attack = undefined;
+    this.defence = undefined;
+  }   
+}
+
+module.exports = Character;
